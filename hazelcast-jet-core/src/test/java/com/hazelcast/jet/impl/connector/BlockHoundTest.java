582d98a93ef5c837ab6438b08e5d9eb330f1a993
Viliam Durina <viliam-durina@users.noreply.github.com>
2020-10-20 09:34:09 +0200

    Fix the BlockHoundTest

--- hazelcast-jet-core/src/test/java/com/hazelcast/jet/impl/connector/BlockHoundTest.java
+++ hazelcast-jet-core/src/test/java/com/hazelcast/jet/impl/connector/BlockHoundTest.java
@@ -7,19 +7,20 @@ import reactor.blockhound.BlockHound;
 import reactor.blockhound.BlockingOperationError;
 import reactor.blockhound.integration.BlockHoundIntegration;
 
-import java.util.concurrent.Callable;
+import javax.annotation.Nonnull;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.FutureTask;
+import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class BlockHoundTest {
 
     @BeforeClass
     public static void setUpClass() {
         BlockHound.builder()
-                  .disallowBlockingCallsInside(Callable.class.getName(), "call")
                   .with(new JetBlockHoundIntegration())
                   .install();
     }
@@ -31,7 +32,14 @@ public class BlockHoundTest {
                 Thread.sleep(0);
                 return "";
             });
-            Executors.newSingleThreadExecutor().execute(task);
+            Executors.newSingleThreadExecutor(new ThreadFactory() {
+                private final AtomicInteger count = new AtomicInteger();
+
+                @Override
+                public Thread newThread(@Nonnull Runnable r) {
+                    return new Thread(r, "my-pool-" + count.getAndIncrement());
+                }
+            }).execute(task);
 
             task.get(10, TimeUnit.SECONDS);
             Assert.fail("should fail");
